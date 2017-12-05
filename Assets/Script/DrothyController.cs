f0c508874058a159cface90613d47b484aa4cc80
meganeroppy <roppy@hotmail.co.jp>
2017-12-06 03:39:16 +0900

    RTSに対応する想定のシーンを作成

---
+++ Assets/Script/DrothyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrothyController : MonoBehaviour 
+{
+	private Animator anim;
+	private bool walking;
+
+	[SerializeField]
+	private float threasholdMoveSpeed = 1f;
+	private Vector3 prevPosition;
+
+	// Use this for initialization
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+		prevPosition = anim.bodyPosition;
+		walking = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		var diff = (prevPosition - anim.bodyPosition).magnitude;
+
+		//Debug.Log( diff );
+
+		walking = diff >= threasholdMoveSpeed;
+
+		if( walking != anim.GetBool("Walking") )
+		{
+			anim.SetBool("Walking", walking);
+		}
+
+		prevPosition = anim.bodyPosition;
+	}
+}
